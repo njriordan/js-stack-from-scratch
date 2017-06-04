7982bce577a1aeb6ef34099ac0e5eda05cda7282
Niall Riordan <nriordan@airware.com>
2017-06-04 13:56:34 +0200

    04 - Introduce Hot Module Replacement

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,8 +4,27 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
 import App from './app'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
-ReactDOM.render(<App />, document.querySelector(APP_CONTAINER_SELECTOR))
+
+const rootElement = document.querySelector(APP_CONTAINER_SELECTOR)
+
+const wrapApp = AppComponent => (
+  <AppContainer>
+    <AppComponent />
+  </AppContainer>
+)
+
+ReactDOM.render(wrapApp(App), rootElement)
+
+if (module.hot) {
+  // flow-disable-next-line
+  module.hot.accept('./app', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('./app').default
+    ReactDOM.render(wrapApp(NextApp), rootElement)
+  })
+}
