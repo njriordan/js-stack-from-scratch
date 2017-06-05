14bdff4018265fafbd7c5e0b8ace94090d8f4a14
Niall Riordan <nriordan@airware.com>
2017-06-05 12:49:46 +0200

    05 - Introduce Redux (first action, first reducer)

---
+++ src/client/reducers/hello.js
@@ -0,0 +1,20 @@
+// @flow
+import Immutable from 'immutable'
+import type { fromJS as Immut } from 'immutable'
+
+import { SAY_HELLO } from '../actions/hello'
+
+const initialState = Immutable.fromJS({
+  message: 'I am the initial state',
+})
+
+const helloReducer = (state: Immut = initialState, action: { type: string, payload: any}) => {
+  switch (action.type) {
+    case SAY_HELLO:
+      return state.set('message', action.payload)
+    default:
+      return state
+  }
+}
+
+export default helloReducer
