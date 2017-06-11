1d4ee2233697b8ef0275e52349eec32b737535e7
Niall Riordan <nriordan@airware.com>
2017-06-11 12:11:50 +0200

    06 - Introduce React Router and page navigation

---
+++ src/client/components/pages/hello.jsx
@@ -0,0 +1,15 @@
+// @flow
+
+import React from 'react'
+
+import HelloButton from '../../containers/hello-button'
+import Message from '../../containers/message'
+
+const HelloPage = () => (
+  <div>
+    <Message />
+    <HelloButton />
+  </div>
+)
+
+export default HelloPage
